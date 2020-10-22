ef1114c066b4e54dcac7403f9128c3c8f91818d7
Timothy Munene <munenegicharu@gmail.com>
2020-10-22 23:55:22 +0300

    Configure start.sh file

---
+++ start.sh
@@ -0,0 +1,8 @@
+DEBUG=False
+ALLOWED_HOSTS='*'
+DB_NAME=d2lucspfaics07
+DB_USER=mggledqhvahxmk
+DB_PASSWORD=e97a235d3370f5af29c13710abfc7e3b99db270298bfb529074d59af2d98e422
+DB_HOST=ec2-52-203-165-126.compute-1.amazonaws.com
+SECRET_KEY=r$*fal==7+1$nh*aqp-xp#*zg)lgr$u!s@g521_py-3*$a2@qz
+DISABLE_COLLECTSTATIC=1
